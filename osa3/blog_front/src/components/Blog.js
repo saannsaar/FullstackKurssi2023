286be03302ae00d120e3cf12964c75383a4ebf6a
saannsaar <sannsaarinen@gmail.com>
2023-05-16 12:56:42 +0300

    5.7
    5.7

--- osa3/blog_front/src/components/Blog.js
+++ osa3/blog_front/src/components/Blog.js
@@ -1,11 +1,31 @@
-const Blog = ({blog}) => (
+
+import { useState } from "react"
+
+const Blog = ({blog}) => {
+
+  const [showMoreInfo, setShowMoreInfo] = useState(false)
+
+  if (!showMoreInfo) {
+    return (
+      <div class="blogi">
+      <h3>{blog.title}, by {blog.author}</h3>
+      <button onClick={() => setShowMoreInfo(true)}>Show</button>
+      </div>
+    )
+  }
+  return (  
   <div class="blogi">
-    <h3>{blog.title}</h3>
-    <p><strong>Author: </strong> {blog.author} </p>
-    <p><strong>URL: </strong> {blog.url} </p>
-    <p> <strong>This blog has: </strong> {blog.likes} likes! </p>
-  </div>  
+ <h3>{blog.title}, by {blog.author}</h3>
+  <p><strong>Author: </strong> {blog.author} </p>
+  <p><strong>URL: </strong> {blog.url} </p>
+  <p> <strong>This blog has: </strong> {blog.likes} <strong> likes!</strong> 
+  <button>Like</button></p>
+<button onClick={() => setShowMoreInfo(false)}>Hide</button>
+</div> 
+
+  )
  
-)
+ 
+  }
 
 export default Blog
