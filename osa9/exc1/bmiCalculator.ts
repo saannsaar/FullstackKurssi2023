50d10b16c56e7819abe9a401f85a0e1b01dd153e
saannsaar <sannsaarinen@gmail.com>
2023-06-15 11:25:15 +0300

    vielä virheilmotus

--- osa9/exc1/bmiCalculator.ts
+++ osa9/exc1/bmiCalculator.ts
@@ -23,20 +23,26 @@
     if( bmi < 16.0 ) {
         return `Underweight (Severe thinness): ${bmi}`
     }
-    if (bmi > 16.0 && bmi < 16.9 ) {
+    else if (bmi > 16.0 && bmi < 16.9 ) {
         return `Underweight (Moderate thinness): ${bmi}`
     }
-    if ( bmi > 17.0 && bmi < 18.4) {
+    else if ( bmi > 17.0 && bmi < 18.4) {
         return `Underweight (Mild thinness): ${bmi}`
     }
-    if (bmi > 18.5 && bmi < 24.9) {
+    else if (bmi > 18.5 && bmi < 24.9) {
         return `Normal range: ${bmi}`
     }
-    if (bmi > 25.0 && bmi < 29.9) {
+    else if (bmi > 25.0 && bmi < 29.9) {
         return `Overweight (Pre-obese): ${bmi}`
     }
-    if ( bmi > 30.0 && bmi < 34.9) {
+    else if ( bmi > 30.0 && bmi < 34.9) {
         return `Obese (Class I): ${bmi}`
+    }
+    else if (bmi > 35.0 && bmi < 39.9) {
+        return  `Obese (Class II): ${bmi}`
+    }
+    else {
+        return `Obese (Class III): ${bmi}`
     }
    
 
@@ -53,3 +59,5 @@
     }
     console.log(errorMessage);
   }
+
+  export { calculateBmi }
--- osa9/exc1/index.ts
+++ osa9/exc1/index.ts
@@ -1,9 +1,19 @@
 import express from 'express'
+import { calculateBmi } from './bmiCalculator'
 
 const app = express()
 
 app.get('/hello', (_req, res) => {
     res.send('Hello Full Stack!')
+})
+
+
+app.get('/bmi', (req, res) => {
+    const weight = req.query.weight
+    const height = req.query.height
+    console.log(weight, height)
+    const bmiResult = calculateBmi(Number(weight), Number(height))
+    res.json({ weight, height, bmi: bmiResult })
 })
 
 const PORT = 3003
